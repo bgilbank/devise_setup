8dbfcd7823c98deeb1eb913f160cca6e4f1a3e3a
Brian Gilbank <bg@briangilbank.com>
2018-01-07 01:26:23 -0700

    got posts working properly in the admin module

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
  
+  namespace :admin do
+    resources :posts
+  end
+
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :posts
   root to: 'posts#index'
